e597d6e4b1af383859185cd8f30009cfaf6fe57e
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-01-19 03:49:10 -0300

    Fixes import of react-native View

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View } from 'react-view';
+import { View } from 'react-native';
 
 function Main() {
     return <View />;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View } from 'react-view';
+import { View } from 'react-native';
 
 function Profile() {
     return <View />;
