bcd22c65a1096715954d1e1a472e1aca4fd320a3
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-01-19 05:19:09 -0300

    Adds form for searching devs

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -33,21 +34,34 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -22.9089988, longitude: -43.3027952 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/58062772?s=460&v=4' }} />
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -22.9089988, longitude: -43.3027952 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/58062772?s=460&v=4' }} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'ovtfernandes' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Vitor Fernandes</Text>
-                        <Text style={styles.devBio}>Oieee</Text>
-                        <Text style={styles.devTechs}>ReactJS, React Native, Nodejs</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'ovtfernandes' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Vitor Fernandes</Text>
+                            <Text style={styles.devBio}>Oieee</Text>
+                            <Text style={styles.devTechs}>ReactJS, React Native, Nodejs</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false} />
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#fff" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -80,6 +94,42 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     },
 });
 
