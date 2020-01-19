3be7ddae17a8d5a125398e058eb93d24c206bd2b
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-01-19 05:08:46 -0300

    Adds profile visualization

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { WebView } from 'react-native-webview';
 
-import { View } from 'react-native';
+function Profile({ navigation }) {
+    const githubUsername = navigation.getParam('github_username');
 
-function Profile() {
-    return <View />;
+    return <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUsername}` }} />;
 }
 
 export default Profile;
